a38972304e9752d4e80870ef96604e623a111ec9
amish1188 <martin.joodd@gmail.com>
2019-10-27 20:19:31 +0100

    Initial commit

---
+++ src/test/java/com/marcin/HttpClientTest.java
@@ -0,0 +1,25 @@
+package com.marcin;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpClientTest {
+
+    @Test
+    void shouldReturnStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", "/echo");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnFailStatusCode() throws IOException {
+        HttpClient client = new HttpClient("urlecho.appspot.com", "/echo?status=401");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(401,response.getStatusCode());
+    }
+
+}
