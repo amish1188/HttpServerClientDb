d4bcffa5648f0fd2be8f8281a47665cd3aafb527
amish1188 <martin.joodd@gmail.com>
2019-10-29 18:39:42 +0100

    fixes everything

--- src/main/java/com/marcin/HttpServer.java
+++ src/main/java/com/marcin/HttpServer.java
@@ -1,82 +1,114 @@
 package com.marcin;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HttpServer {
 
-    private static ServerSocket serverSocket;
+    private int port;
+    private ServerSocket serverSocket;
+    private String fileLocation;
 
     public HttpServer(int port) throws IOException {
+        this.port = port;
         serverSocket = new ServerSocket(port);
+
     }
 
     public static void main(String[] args) throws IOException {
-        new HttpServer(8080).start();
+        HttpServer httpServer = new HttpServer(8080);
+        httpServer.setFileLocation("src/main/resources");
+        httpServer.startServer();
     }
 
-    public static void start() throws IOException {
-        while(true) {
-            Socket socket = serverSocket.accept();
-            String statusLine = readLine(socket);
-            String line;
-            while(!(line= readLine(socket)).isEmpty()) {
+    public void startServer() throws IOException {
+        new Thread(() -> run()).start();
+    }
 
-                System.out.println("Line: " + line);
-            }
-
-            System.out.println("Done");
-
-            String requestTarget = statusLine.split(" ")[1];
-            int questionPos = requestTarget.indexOf('?');
-            String statusCode = "200";
-            String location = null;
-            String body = "Hello World!";
-
-            if(questionPos > 0) {
-                String query = requestTarget.substring(questionPos + 1);
-                int equalsPos = query.indexOf('=');
-                String paramName = query.substring(0, equalsPos);
-                String paramValue = query.substring(equalsPos + 1);
-                if(paramName.equals("status")) {
-                    statusCode = paramValue;
-                } else if(paramName.equals("Location")) {
-                    location = paramValue;
-                } else if(paramName.equals("body")) {
-                    body = paramValue;
+    private void run() {
+        while (true) {
+            try (Socket socket = serverSocket.accept()) {
+                StringBuilder line = new StringBuilder();
+                String requestLine = null;
+                int c;
+                while ((c = socket.getInputStream().read()) != -1) {
+                    if (c == '\r') {
+                        c = socket.getInputStream().read();
+                        if(requestLine == null) {
+                            requestLine = line.toString();
+                        }
+                        System.out.println(line);
+                        if (line.toString().isBlank()) {
+                            break;
+                        }
+                        line = new StringBuilder();
+                    }
+                    line.append((char) c);
                 }
-            }
 
-            socket.getOutputStream().write(("HTTP/1.1 " + statusCode + " OK\r\n").getBytes());
-            socket.getOutputStream().write("Content-Type: text/html; charset=utf-8\r\n".getBytes());
-            socket.getOutputStream().write(("Content-Length: "+ body.length() + "\r\n").getBytes());
-            socket.getOutputStream().write("Connection: close\r\n".getBytes());
-            if(location != null){
-                socket.getOutputStream().write(("Location: " + location + "\r\n").getBytes());
+                String requestTarget = requestLine.split(" ")[1];
+
+                int questionPos = requestTarget.indexOf("?");
+                String requestPath = questionPos == -1 ? requestTarget : requestTarget.substring(0, questionPos);
+                if(!requestPath.equals("/echo")) {
+                    File file= new File(fileLocation + requestPath);
+                    socket.getOutputStream().write(("HTTP/1.1 200 OK\r\n" +
+                                        "Content-Length: " + file.length() + "\r\n" +
+                                        "Connection: close\r\n" +
+                                        "\r\n").getBytes());
+                    new FileInputStream(file).transferTo(socket.getOutputStream());
+                }
+
+
+
+                Map<String, String> queryParameters = parseQueryParameters(requestTarget);
+
+                String status = queryParameters.getOrDefault("status", "200");
+                String location = queryParameters.getOrDefault("Location", null);
+                String body = queryParameters.getOrDefault("body", "Hello World");
+
+                socket.getOutputStream().write(("HTTP/1.1 " + status + " OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: " + body.length() + "\r\n" +
+                        "Connection: close\r\n" +
+                        (location != null ? "Location: " + location + "\r\n" : "") +
+                        "\r\n" +
+                        body).getBytes());
+                socket.getOutputStream().flush();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            socket.getOutputStream().write("\r\n".getBytes());
-            socket.getOutputStream().write((body + "\r\n").getBytes());
         }
     }
 
-    private static String readLine(Socket socket) throws IOException {
-        int c;
-        StringBuilder line = new StringBuilder();
-        while((c = socket.getInputStream().read()) != -1) {
-            if(c == '\r') {
-                c = socket.getInputStream().read();
-                if (c != '\n') {
-                    System.err.println("Unexpected character! " + ((char) c));
-                }
-                return line.toString();
+    private Map<String, String> parseQueryParameters(String requestTarget) {
+        Map <String, String> queryParameters = new HashMap<>();
+        int questionPos = requestTarget.indexOf("?");
+        if(questionPos > 0) {
+            String query = requestTarget.substring(questionPos+1);
+            for(String parameter : query.split("&")) {
+                int equalsPos = parameter.indexOf("=");
+                String paramName = parameter.substring(0, equalsPos);
+                String paramValue = parameter.substring(equalsPos+1);
+                queryParameters.put(paramName, paramValue);
             }
-            line.append((char)c);
         }
-        return line.toString();
+        return queryParameters;
     }
 
-    public int getActualPort() {
+    public int getPort(){
         return serverSocket.getLocalPort();
     }
+
+    public void setFileLocation(String fileLocation) {
+        this.fileLocation = fileLocation;
+    }
 }
+
+
+
--- src/test/java/com/marcin/HttpServerTest.java
+++ src/test/java/com/marcin/HttpServerTest.java
@@ -1,24 +1,78 @@
 package com.marcin;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class HttpServerTest {
+
+
+
+     private HttpServer server;
+
+    @BeforeEach
+    void setUp() throws IOException {
+        server = new HttpServer(0);
+        server.startServer();
+    }
+
+
+    @Test
+    void shouldReturn200() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(200, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturn401() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?status=401");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals(401, response.getStatusCode());
+    }
+
+    @Test
+    void shouldReturnResponseHeader() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?status=302&Location=http://www.example.com");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals("http://www.example.com", response.getHeader("Location"));
+    }
+
+    @Test
+    void shouldReturnContent() throws IOException {
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?body=HiKutas");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals("7", response.getHeader("Content-Length"));
+        assertEquals("HiKutas", response.getBody());
+    }
+
+    @Test
+    void shoulFileFromDisk() throws IOException {
+        Files.writeString(Paths.get("target/mytestfile.txt"), "Hello Kristiania");
+        server.setFileLocation("target");
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/mytestfile.txt");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals("Hello Kristiania", response.getBody());
+    }
+
+
+    /*
     @Test
     void shouldReturnFailStatusCode() throws IOException {
-        int port = startServer();
-        HttpClient client = new HttpClient("localhost", port, "/echo?status=401");
+
+        HttpClient client = new HttpClient("localhost", server.getActualPort(), "/echo?status=401");
         HttpClientResponse response = client.executeRequest();
         assertEquals(401,response.getStatusCode());
     }
 
     @Test
     void shouldReturnRequestedHeader() throws IOException {
-        int port = startServer();
-        HttpClient client = new HttpClient("localhost", port, "/echo?Location=http://example.com");
+        HttpClient client = new HttpClient("localhost", server.getActualPort(), "/echo?Location=http://example.com");
         HttpClientResponse response = client.executeRequest();
         assertEquals(200,response.getStatusCode());
         assertEquals("http://example.com",response.getHeader("Location"));
@@ -26,23 +80,18 @@ public class HttpServerTest {
 
     @Test
     void shouldReturnRequestedBody() throws IOException {
-        int port = startServer();
-        HttpClient client = new HttpClient("localhost", port, "/echo?body=HelloKutas");
+        HttpClient client = new HttpClient("localhost", server.getActualPort(), "/echo?body=HelloKutas");
         HttpClientResponse response = client.executeRequest();
         assertEquals("HelloKutas",response.getBody());
     }
 
-    private int startServer() throws IOException {
-        HttpServer httpServer = new HttpServer(0);
-
-        new Thread(() -> {
-            try {
-                httpServer.start();
-            } catch (IOException e){
-                e.printStackTrace();
-            }
-        }).start();
-        return httpServer.getActualPort();
-    }
+    @Test
+    void shouldReturnFileFromDisk() throws IOException {
+        Files.writeString(Paths.get("target/mytestfile.txt"), "Hello Kristiania");
+        server.setFileLocation("target");
+        HttpClient client = new HttpClient("localhost", server.getActualPort(),"/mytestfile.txt");
+        HttpClientResponse response = client.executeRequest();
+        assertEquals("Hello Kristiania", response.getBody());
+    } */
 
 }
