4d1b3e0a4ae9490e26f23373a7ec61e79496e12e
amish1188 <martin.joodd@gmail.com>
2019-10-30 11:35:36 +0100

    repairs tests by changing '/echo' to '/'

--- src/test/java/com/marcin/HttpServerTest.java
+++ src/test/java/com/marcin/HttpServerTest.java
@@ -24,28 +24,28 @@ public class HttpServerTest {
 
     @Test
     void shouldReturn200() throws IOException {
-        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo");
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/");
         HttpClientResponse response = client.executeRequest();
         assertEquals(200, response.getStatusCode());
     }
 
     @Test
     void shouldReturn401() throws IOException {
-        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?status=401");
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/?status=401");
         HttpClientResponse response = client.executeRequest();
         assertEquals(401, response.getStatusCode());
     }
 
     @Test
     void shouldReturnResponseHeader() throws IOException {
-        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?status=302&Location=http://www.example.com");
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/?status=302&Location=http://www.example.com");
         HttpClientResponse response = client.executeRequest();
         assertEquals("http://www.example.com", response.getHeader("Location"));
     }
 
     @Test
     void shouldReturnContent() throws IOException {
-        HttpClient client = new HttpClient("localhost", server.getPort(), "/echo?body=HiKutas");
+        HttpClient client = new HttpClient("localhost", server.getPort(), "/?body=HiKutas");
         HttpClientResponse response = client.executeRequest();
         assertEquals("7", response.getHeader("Content-Length"));
         assertEquals("HiKutas", response.getBody());
